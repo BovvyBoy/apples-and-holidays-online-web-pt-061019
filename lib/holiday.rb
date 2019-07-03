2a669c28aa5b15cc2b9e40d89ecb2849bcfa9ea3
Daniel Bovington <d.bovington@gmail.com>
2019-07-03 15:57:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday_name, items|
       if items.include?("BBQ")
-      
+        return holiday_name
+      end
     end
   end
 end
